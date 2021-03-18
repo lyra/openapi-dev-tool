330aca55ed15d4779ee82a9ae2f88d528a9831f2
Damien Guérin <damien.guerin@lyra-network.com>
2021-03-18 16:51:37 +0100

    Add getPOMContent UT

--- tests/lib/utils.spec.js
+++ tests/lib/utils.spec.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 import chai from 'chai';
 import chaiString from 'chai-string';
-import { isYAMLFile, isJSONFile, getTempDir } from '../../src/lib/utils';
+import { isYAMLFile, isJSONFile, getTempDir, getPOMContent } from '../../src/lib/utils';
 
 chai.use(chaiString);
 
@@ -37,6 +37,22 @@ describe('utils.js file', function() {
     it("should start with 'openapi-dev-tool_'", function() {
       const tmp = getTempDir();
       assert.startsWith(path.basename(tmp.name), 'openapi-dev-tool_');
+    });
+  });
+
+  describe('getPOMContent function', function() {
+    it("should return xml with artifact data", function() {
+      const content = getPOMContent('artifactId', 'version', 'groupId', 'packaging');
+
+      let expected = '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">\n';
+      expected += '\t<modelVersion>4.0.0</modelVersion>\n';
+      expected += `\t<groupId>groupId</groupId>\n`;
+      expected += `\t<artifactId>artifactId</artifactId>\n`;
+      expected += `\t<version>version</version>\n`;
+      expected += `\t<packaging>packaging</packaging>\n`;
+      expected += '</project>';
+
+      assert.equal(content, expected);
     });
   });
 });
