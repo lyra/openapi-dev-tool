5889c4f494524eee12db0f4d18d2601a85fde216
Damien Guérin <damien.guerin@lyra-network.com>
2023-12-18 16:33:46 +0100

    Add tests on archiver

--- tests/lib/archiver.spec.js
+++ tests/lib/archiver.spec.js
@@ -1,4 +1,8 @@
 import path from 'path';
+import StreamZip from 'node-stream-zip';
+import StreamTar from 'tar-stream';
+import fs from 'fs';
+import zlib from 'node:zlib';
 
 import { generateSpecsArchive } from '../../src/lib/archiver.js';
 
@@ -11,8 +15,21 @@ describe('archiver.js file', () => {
           title: 'title',
         },
       };
-      const res = await generateSpecsArchive(api, ['/dir/file.yaml'], 'maven');
-      expect(path.basename(res)).toBe('title-1.0.0.zip');
+      const res = await generateSpecsArchive(
+        api,
+        ['./tests/assets/specs.yaml'],
+        'maven'
+      );
+      const zip = new StreamZip({
+        file: res,
+        storeEntries: true,
+      });
+      zip.on('ready', () => {
+        expect(zip.entriesCount).toBe(1);
+        expect(Object.values(zip.entries())[0].name).toBe('specs.yaml');
+        expect(path.basename(res)).toBe('title-1.0.0.zip');
+        zip.close();
+      });
     });
 
     it('should return a tar archive file when the input is correct', async () => {
@@ -22,8 +39,18 @@ describe('archiver.js file', () => {
           title: 'title',
         },
       };
-      const res = await generateSpecsArchive(api, ['/dir/file.yaml'], 'npm');
-      expect(path.basename(res)).toBe('title-1.0.0.tar');
+      const res = await generateSpecsArchive(
+        api,
+        ['./tests/assets/specs.yaml'],
+        'npm'
+      );
+      const extract = StreamTar.extract();
+      fs.createReadStream(res).pipe(zlib.createGunzip()).pipe(extract);
+      extract.on('entry', function (header, stream, next) {
+        expect(header.name).toBe('package/specs.yaml');
+        expect(next()).toBe(undefined);
+        expect(path.basename(res)).toBe('title-1.0.0.tar');
+      });
     });
 
     it('should return a archive file when the title seems incorrect', async () => {
